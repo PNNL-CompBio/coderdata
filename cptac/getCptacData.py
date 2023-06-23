eb67c7dae13f2523494810df396d453a2183f022
Sara JC Gosline <sara.gosline@pnnl.gov>
2023-06-23 09:09:34 -0700

    updated

--- cptac/getCptacData.py
+++ cptac/getCptacData.py
@@ -5,7 +5,7 @@ Basic CLI to import CPTAC protegenomic data
 import argparse
 import cptac
 import os
-
+import numpy as np
 import pandas as pd
 
 
@@ -17,7 +17,7 @@ if os.path.exists('./samples.csv'):
     samples = pd.read_csv('./samples.csv')
     maxval = max(samples.improve_sample_id)
 else:
-    samples = pd.DataFrame({'common_name':[],'cancer_type':[],'other_names':[],'species':[],\
+    samples = pd.DataFrame.from_dict({'common_name':[],'cancer_type':[],'other_names':[],'species':[],\
                             'improve_sample_id':[],'id_source':[],'other_id':[],'model_type':[]})
     maxval = max(pd.read_csv('https://raw.githubusercontent.com/PNNL-CompBio/candleDataProcessing/main/data/samples.csv').improve_sample_id)
     
@@ -42,6 +42,8 @@ def getCancerObj(cancertype):
         dat = cptac.Luad()
     elif cancertype == 'ovarian':
         dat = cptac.Ovarian()
+    elif cancertype =='pdac':
+        dat = cptac.Pdac()
     else:
         exit()
     return dat
@@ -49,19 +51,27 @@ def getCancerObj(cancertype):
 
 ct_vals={'brca':'Breast carcinoma','ccrcc':'Clear cell renal cell carcinoma','colon':'Colon adenocarcinoma',\
          'endometrial':'Endometrial carcinoma','gbm':'Glioblastoma','hnscc':'Head and neck squamous cell carcinoma',\
-         'lscc':'Lung squamous cell carcinoma','luad':'Lung adenocarcinoma','ovarian':'Ovarian carcinoma'}
+         'lscc':'Lung squamous cell carcinoma','luad':'Lung adenocarcinoma','ovarian':'Ovarian carcinoma',\
+         'pdac': 'Pancreatic ductal adenocarcinoma'}
 def buildTumorSampleTable(sample_names,cancer_type):
     '''
     we have to read in existing sample information first to see if it is part of the samples
     '''
+    global maxval,samples
+    
     for samp in sample_names:
        if samp not in samples.common_name:
-           maxval = maxval+1
-           samples.append({'common_name':samp,'cancer_type':ct_vals[cancer_type],'other_names':'',\
-                           species:'Homo Sapiens (Human)',\
-                           'improve_sample_id':maxval,'id_source':'CPTAC3',\
-                           'other_id':samp,'model_type':'Tumor'})
-    samples.write_csv('samples.csv')
+           maxval = int(maxval+1)
+           samples = pd.concat([samples,
+                                pd.DataFrame({'common_name':[samp],'cancer_type':[ct_vals[cancer_type]],'other_names':[''],\
+                                              'species':['Homo Sapiens (Human)'],\
+                                              'improve_sample_id':[maxval],'id_source':['CPTAC3'],\
+                                              'other_id':[samp],'model_type':['Tumor']}).reset_index(drop=True)])
+
+
+    samples = samples.reset_index(drop=True)
+    #print(samples)
+    samples.to_csv('samples.csv',index=False)
     return samples
    
    
@@ -86,12 +96,16 @@ def formatData(df,dtype,ctype,samp_names):
 
     #     snames=list(set(longdf.Patient_ID))
     improve_mapping = buildTumorSampleTable(samp_names,ctype)
+    improve_mapping.reset_index(drop=True)
     improve_mapping.index=improve_mapping.common_name
-    blongdf = longdf.join(improve_mapping,on='Patient_ID'))
-     
+
+    blongdf = longdf.join(improve_mapping,on='Patient_ID')
+    improve_mapping.reset_index(drop=True)
     ##match gene identifiers
+
     genes.index=genes.gene_symbol
-    mlongdf = nlongdf.join(genes,on='gene_symbol',how='left',lsuffix='.e',rsuffix='.m')) ##fix this
+    mlongdf = blongdf.join(genes,on='gene_symbol',how='left',lsuffix='.e',rsuffix='.m') ##fix this
+    return mlongdf[['entrez_id','improve_sample_id',dtype]].drop_duplicates()
 
      
 
@@ -104,13 +118,16 @@ def main():
    #                     help='Sample type, tumor vs normal vs all (default), \
    #                     to be collected')
    # opts = parser.parse_args()
+    dat_files = {}
 
-    for cancertype in ['brca','endometrial','ccrcc','hnscc','lscc','luad','ovarian']:
+    for cancertype in ['brca','endometrial','ccrcc','hnscc','lscc','luad','ovarian','gbm','colon','pdac']:
         dat = getCancerObj(cancertype)
 
-        ##get the tumors
+        ##get the tumor sample identifiers
         tumor_samps = dat.get_clinical().Sample_Tumor_Normal=='Tumor'
         tumor_samps = list(tumor_samps.index)
+
+        ##get available data for cancer
         dat_list=dat.get_data_list()
         all_dfs = {}
         for dtype in ['proteomics','transcriptomics','somatic_mutations','miRNA','CNV']:
@@ -128,35 +145,23 @@ def main():
                 all_dfs[dtype] = dat.get_CNV()
 
         for dtype,df in all_dfs.items():
-#       df = df.loc[ind]
+            df = df.loc[[t for t in tumor_samps if t in df.index]] ##get the data for those samples
             dfE = np.exp(df)
             dfU = np.log(dfE.sum(axis=1, level=0, min_count=1))
             dfU.dropna(how='all', axis=0, inplace=True)
-
-            ##now we move to long form and correct
-            fdf = formatData(dfU,dtype,cancertype,samples)
-            fname=cancertype+'_'+dtype+'.csv'
-            dfU.transpose().to_csv(path_or_buf=fname, sep=',')
-
-        
-#    sample_type='all'
-    # Get the sample type specific dataframe
-#    if opts.sample.lower() != 'all':
-#        if opts.sample.lower() == 'tumor':
-#            ind = meta[meta["Sample_Tumor_Normal"] == "Tumor"].index
-#            ind = [i for i in ind if i in df.index]
-#            df = df.loc[ind]
-#        elif opts.sample.lower() == 'normal':
-#            nIDs = list(meta[meta["Sample_Tumor_Normal"] == "Normal"].index)
-#            nIDs = list(set(nIDs) & set(df.index))
-#            df = df.loc[nIDs]
-            #df.index = [nID[:-2] if nID[-2:] ==
-            #            ".N" else nID for nID in nIDs]
-#        else:
-#            exit("The sample type, tumor vs normal vs all (default), \
-#            is not correctly set!")
-
+            print(cancertype+' '+dtype)
+            ##now we move to long form and match identifiers
+            fdf = formatData(dfU,dtype,cancertype,tumor_samps).reset_index(drop=True)
+            if dtype in dat_files.keys():
+                of = dat_files[dtype]
+                fdf = pd.concat([of,fdf])
+            dat_files[dtype] = fdf
+    ##now concatenate all the cancers into a single file
+    for dtype,df in dat_files.items():
+        df.to_csv(dtype+'.csv',sep=',',index=False)
+    #fdf.to_csv(path_or_buf=fname, sep=',',index=False)
    
 
 if __name__ == '__main__':
     main()
+
