8f524c3ce6f47a55866fc4cc1c661eb71a0a1e24
Jeremy <jeremy.jacobson3402@gmail.com>
2023-11-19 12:26:04 -0800

    Python Package in progress. Download and Load functions added. Tests added for functions. Pypi repo created. License and setup.py added.

---
+++ tests/test_download_cptac_cli.py
@@ -0,0 +1,32 @@
+# tests/test_download_cptac_cli.py
+
+import subprocess
+import tempfile
+import os
+import glob
+
+def test_cli_download():
+    # Create a temporary directory
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # Change the current working directory to the temporary directory
+        os.chdir(temp_dir)
+
+        # Run the command line tool
+        subprocess.run(['coderdata', 'download', '--prefix', 'cptac'], check=True)
+
+        # Check if the expected files are downloaded
+        cptac_copy_number = glob.glob('cptac_copy_number*')
+        assert len(cptac_copy_number) > 0, "File cptac_copy_number does not exist."
+        
+        cptac_proteomics = glob.glob('cptac_proteomics*')
+        assert len(cptac_proteomics) > 0, "File cptac_proteomics does not exist."
+        
+        cptac_samples = glob.glob('cptac_samples*')
+        assert len(cptac_samples) > 0, "File cptac_samples does not exist."
+        
+        cptac_mutations = glob.glob('cptac_mutations*')
+        assert len(cptac_mutations) > 0, "File cptac_mutations does not exist."
+        
+        cptac_transcriptomics = glob.glob('cptac_transcriptomics*')
+        assert len(cptac_transcriptomics) > 0, "File cptac_transcriptomics does not exist."
+        
