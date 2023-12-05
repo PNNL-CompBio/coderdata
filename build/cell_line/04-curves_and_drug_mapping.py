c94a90c1d323aba5a64e6f2c3a460bbf98d6c2e6
Sara JC Gosline <sara.gosline@pnnl.gov>
2023-12-05 15:34:30 -0800

    updating sanger and docker build

--- build/cell_line/04-curves_and_drug_mapping.py
+++ build/cell_line/04-curves_and_drug_mapping.py
@@ -10,21 +10,18 @@ TODO: add in R or dockerize requirements
 
 '''
 
-
 import os
-
 
 ########Step 4b fit curves
 allfiles=[a for a in os.listdir('./') if 'DoseResponse' in a]
 print(allfiles)
 for a in allfiles:
-    os.system('python ../utils/fit_curve.py --input '+a+' --output '+a)
+    os.system('python fit_curve.py --input '+a+' --output '+a)
 
 ###step 5 concatenate all files
 os.system('cat *.0 > experiments_orig.tsv')
 
 ##now fix drug identifiers in experiments and drug files
 
-
 ### step 7 store files (figshare? ftp?
-os.system('Rscript ../utils/remapDrugsToSmiles.R drugs.tsv.gz experiments_orig.tsv')
+os.system('Rscript remapDrugsToSmiles.R drugs.tsv.gz experiments_orig.tsv')
