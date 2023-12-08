e4a0aa2c238f090e3de201865db19c6e0935a2b5
Sara JC Gosline <sara.gosline@pnnl.gov>
2023-12-08 08:23:17 -0800

    more file moves, and cptac docker updated

--- build/cell_line/03-curves_and_drug_mapping.py
+++ build/cell_line/03-curves_and_drug_mapping.py
@@ -12,16 +12,20 @@ TODO: add in R or dockerize requirements
 
 import os
 
-########Step 4b fit curves
+####step 3a - get dose response data
+cmd = 'Rscript 03a-drugAndResponseData.R'
+os.system(cmd)
+
+########Step 3b fit curves
 allfiles=[a for a in os.listdir('./') if 'DoseResponse' in a]
 print(allfiles)
 for a in allfiles:
     os.system('python fit_curve.py --input '+a+' --output '+a)
 
-###step 5 concatenate all files
+###step 3c concatenate all files
 os.system('cat *.0 > experiments_orig.tsv')
 
 ##now fix drug identifiers in experiments and drug files
 
-### step 7 store files (figshare? ftp?
+### step 3d store files (figshare? ftp?
 os.system('Rscript remapDrugsToSmiles.R drugs.tsv.gz experiments_orig.tsv')
