e9c65c36da0b25f0006c832e23684ef874ffec15
jjacobson95 <jeremy.jacobson3402@gmail.com>
2024-01-19 20:45:55 +0000

    update downloader.py

--- coderdata/download/downloader.py
+++ coderdata/download/downloader.py
@@ -28,7 +28,7 @@ def download_data_by_prefix(dataset_prefix=None):
     """
     
     # Get the dataset details
-    url = "https://api.figshare.com/v2/articles/25016597"
+    url = "https://api.figshare.com/v2/articles/25030490"
     
     response = requests.get(url)
     if response.status_code != 200:
