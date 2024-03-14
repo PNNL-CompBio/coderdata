725e496f2101fb2acdb154f456901f0d588a3790
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-03-13 17:14:48 -0700

    cleaned up build script for better debugging

--- scripts/build_locally.py
+++ scripts/build_locally.py
@@ -7,6 +7,20 @@ import os
 import argparse
 import subprocess
 
+def run_cmd(cmd_arr,filename):
+    print('running...'+filename)
+    env = os.environ.copy()
+    docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','--platform=linux/amd64']
+    cmd = docker_run+cmd_arr
+    #print(cmd)
+    res = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    if res.returncode !=0:
+        print(res.stderr)
+        exit(filename+' samples file failed')
+    else:
+        print(filename+' retrieved')
+            
+
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument('--docker',dest='docker',default=False,action='store_true')
@@ -22,9 +36,9 @@ def main():
     if not os.path.exists('local'):
         os.mkdir('local')
 
-    env = os.environ.copy()
+
     #docker_run='docker run -v $PWD/local/:/tmp/ --platform=linux/amd64 '
-    docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','--platform=linux/amd64']
+
     ###build docker images
     ###
     if args.docker or args.all:
@@ -44,112 +58,53 @@ def main():
     ## can be run independently but first before omics/experiemnts
     if args.samples or args.all:
         ### build gene file
-        cmd = docker_run+['depmap','Rscript','00-buildGeneFile.R']
-        print(cmd)
-        res = subprocess.run(cmd)
+        run_cmd(['depmap','Rscript','00-buildGeneFile.R'],'gene file')
         
         ###build sample files
-        cmd = docker_run+['depmap','Rscript','01-depmapSamples.R']
-        print(cmd)
-        res = subprocess.run(cmd)
-        # os.system(cmd)
+        run_cmd(['depmap','Rscript','01-depmapSamples.R'],'DepMap Sanger Samples')
+        run_cmd(['depmap','Rscript','01a-pullSamples_LINCS.R','/tmp/depmap_samples.csv'],'LINCS samples')
+        run_cmd(['cptac','--geneFile=/tmp/genes.csv','--prevSampleFile=/tmp/depmap_samples.csv'],'cptac samples')
+        run_cmd(['hcmi','python','01-createHCMISamplesFile.py','--samples','/tmp/cptac_samples.csv'],'hcmi samples')
+        run_cmd(['beataml','python','GetBeatAML.py','--token',env['SYNAPSE_AUTH_TOKEN'],'--samples','/tmp/hcmi_samples.csv'],'beatAML samples')
+        run_cmd(['mpnst','Rscript','00_sample_gen.R','/tmp/beataml_samples.csv',env['SYNAPSE_AUTH_TOKEN']],'mpnst samples')
 
-        cmd = docker_run+['depmap','Rscript','01a-pullSamples_LINCS.R','/tmp/depmap_samples.csv']
-        print(cmd)
-        res = subprocess.run(cmd)
-        
-        cmd = docker_run+['cptac','--geneFile=/tmp/genes.csv','--prevSampleFile=/tmp/depmap_samples.csv']
-        print(cmd)
-        res = subprocess.run(cmd)
-        
-        cmd =docker_run+['hcmi','python','01-createHCMISamplesFile.py','--samples=/tmp/cptac_samples.csv']
-        print(cmd)
-        res = subprocess.run(cmd)
-        
-        cmd=docker_run+['beataml','python','GetBeatAML.py','--token','$SYNAPSE_AUTH_TOKEN','--samples=/tmp/hcmi_samples.csv']
-        print(cmd)
-        res = subprocess.run(cmd)
-        
-        cmd = docker_run+['mpnst','Rscript','00_sample_gen.R','/tmp/beataml_samples.csv',env['SYNAPSE_AUTH_TOKEN']]
-        print(cmd)
-        res = subprocess.run(cmd)
 
      ### Drug matching scripts take a while
     ### they are their own step and can be run independentyly, before others, or alongside sample/omics
     ### DepMap/Sanger, MPNST, LINCS
     if args.drugs or args.all:
         ###build drug data
-        dcmd=docker_run+['depmap','Rscript','03-createDrugFile.R','CTRPv2,GDSC,gCSI,PRISM,CCLE,FIMM,NCI60']
-        print(dcmd)
-        res = subprocess.run(dcmd)
+        run_cmd(['depmap','Rscript','03-createDrugFile.R','CTRPv2,GDSC,gCSI,PRISM,CCLE,FIMM,NCI60'],'cell line drugs')
+        run_cmd(['mpnst','Rscript','02_get_drug_data.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/depmap/drugs.tsv'],'mpnst drugs')
+        run_cmd(['depmap','/opt/venv/bin/python','01b-pullDrugs_LINCS.py','--drugFile','/tmp/drugs.tsv.gz'],'LINCS drugs')
 
-        emd = docker_run+['mpnst','Rscript','02_get_drug_data.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/depmap/drugs.tsv']
-        res = subprocess.run(emd)
-        
-        ##build experimental data
-        ecmd= docker_run+['depmap','/opt/venv/bin/python','01b-pullDrugs_LINCS.py','--drugFile','/tmp/drugs.tsv.gz']
-        print(ecmd)
-        res = subprocess.run(ecmd)
-        
 
     #### Any new omics files are created here.
     ## depends on samples!
     ### these are not order dependent but require gene and sample files
     if args.omics or args.all:
-        ##omics data
-
         ###depmap cell line
-        ocmd = docker_run+['depmap','Rscript','02-pullDepMap.R','/tmp/genes.csv','/tmp/depmap_samples.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-        
-        ocmd = docker_run+['depmap','Rscript','02b-pullSanger.R','/tmp/genes.csv','/tmp/depmap_samples.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-
-        ocmd = docker_run+['depmap','/opt/venv/bin/python3','02a-depMapProts.py','--gene','/tmp/genes.csv','--sample','/tmp/depmap_samples.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-        
+        run_cmd(['depmap','Rscript','02-pullDepMap.R','/tmp/genes.csv','/tmp/depmap_samples.csv'],'depmap omics')
+        run_cmd(['depmap','Rscript','02b-pullSanger.R','/tmp/genes.csv','/tmp/depmap_samples.csv'],'sanger omics')
+        run_cmd(['depmap','/opt/venv/bin/python3','02a-depMapProts.py','--gene','/tmp/genes.csv','--sample','/tmp/depmap_samples.csv'],'depmap proteomics')
         ###cptac
-        ocmd = docker_run+['cptac','--geneFile','/tmp/genes.csv','--curSampleFile','/tmp/cptac_samples.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-        
+        run_cmd(['cptac','--geneFile','/tmp/genes.csv','--curSampleFile','/tmp/cptac_samples.csv'],'cptac omics')
         ###HCMI - the folowing three steps are all required?
         for dt in ['transcriptomics','copy_number','mutations']:
-            ocmd = docker_run+['hcmi','/opt/venv/bin/python','02-getHCMIData.py','-m','full_manifest.txt ','-t',dt,'-o','hcmi_'+dt+'.csv']
-            print(ocmd)
-            res = subprocess.run(ocmd)
-        
-
+            run_cmd(['hcmi','/opt/venv/bin/python','02-getHCMIData.py','-m','full_manifest.txt ','-t',dt,'-o','hcmi_'+dt+'.csv'],\
+                    'hcmi '+dt+' omics')
         ##beataml
-        ocmd=docker_run+['beataml','python','GetBeatAML.py','--token' ,env['SYNAPSE_AUTH_TOKEN'],'--samples','/tmp/beataml_samples.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-        
-
+        run_cmd(['beataml','python','GetBeatAML.py','--token' ,env['SYNAPSE_AUTH_TOKEN'],'--samples','/tmp/beataml_samples.csv'],'beatAML omics')
         ###mpnst
-        ocmd=docker_run+['mpnst','Rscript','01_mpnst_get_omics.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/MPNST_samples.csv','/tmp/genes.csv']
-        print(ocmd)
-        res = subprocess.run(ocmd)
-
+        run_cmd(['mpnst','Rscript','01_mpnst_get_omics.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/MPNST_samples.csv','/tmp/genes.csv'],'MPNST omics')
 
     ### drug response data
     ## requires samplesa nd drugs to complete
     if args.exp or args.all:
-            
-        dcmd=docker_run+['depmap','/opt/venv/bin/python','04-drug_dosage_and_curves.py','--drugfile','/tmp/drugs.tsv','--curSampleFile','/tmp/depmap_samples.csv']
-        print(dcmd)
-        res = subprocess.run(dcmd)
-
-        dcmd = docker_run+['mpnst','Rscript','03_get_drug_response_data.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/MPNST_samples.csv','/tmp/depmap/drugs.tsv']
-        print(dcmd)
-        res = subprocess.run(dcmd)
+        run_cmd(['depmap','/opt/venv/bin/python','04-drug_dosage_and_curves.py','--drugfile','/tmp/drugs.tsv','--curSampleFile','/tmp/depmap_samples.csv'],'cell line experiments')
+        run_cmd(['mpnst','Rscript','03_get_drug_response_data.R',env['SYNAPSE_AUTH_TOKEN'],'/tmp/MPNST_samples.csv','/tmp/depmap/drugs.tsv'],'MPNST experiments')
+        run_cmd(['cell-line Rscript','05-LINCS_perturbations.R','/tmp/genes.csv','/tmp/drugs.tsv','/tmp/depmap_samples.csv'],'LINCS perturbations')
         
-
-        ecmd = docker_run+['cell-line Rscript','05-LINCS_perturbations.R','/tmp/genes.csv','/tmp/drugs.tsv.gz','/tmp/depmap_samples.csv']
-        res = subprocess.run(ecmd)
 
 
 
