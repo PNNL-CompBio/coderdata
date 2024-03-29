39db9c4cc2c65e32f494ebc675dfeec8a53056a3
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-03-29 09:55:52 -0700

    gzipped files

--- build/lincs/requirements.r
+++ build/lincs/requirements.r
@@ -1,5 +1,8 @@
+install.packages('BiocManager')
+install.packages("remotes")
+remotes::install_github("RGLab/cytolib")
+BiocManager::install("cmapR",update=TRUE,ask=FALSE)
 install.packages("readr")
 install.packages("tidyr")
 install.packages("dplyr")
-install.packages('BiocManager')
-BiocManager::install("cmapR",update=TRUE,ask=FALSE)
+
