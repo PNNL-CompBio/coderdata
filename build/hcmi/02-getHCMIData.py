c0a3cc61e9174f522beffe5c71effdde22c9f8d1
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-03-29 09:59:14 -0700

    gzipped

--- build/hcmi/02-getHCMIData.py
+++ build/hcmi/02-getHCMIData.py
@@ -475,7 +475,10 @@ def write_dataframe_to_csv(dataframe, outname):
     -------
     None
     """
-    dataframe.write_csv(outname)
+    if('gz' in outname):
+        dataframe.write_csv(outname,compression='gzip')
+    else:
+        dataframe.write_csv(outname)
     return
 
 def upload_to_figshare(token, title, filepath):
