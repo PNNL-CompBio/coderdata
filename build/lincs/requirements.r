a26989c976ceb88126125a21bc3f8ffd51f1c4bd
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-03-29 14:11:55 -0700

    updated to fix #124

--- build/lincs/requirements.r
+++ build/lincs/requirements.r
@@ -1,5 +1,6 @@
 install.packages('BiocManager')
 install.packages("remotes")
+BiocManager::install(version = "3.16")
 remotes::install_github("RGLab/cytolib")
 BiocManager::install("cmapR",update=TRUE,ask=FALSE)
 install.packages("readr")
