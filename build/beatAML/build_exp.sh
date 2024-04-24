ebc731354772f5027d1dc6d60d408b8d7d5dc1dd
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-04-23 21:25:23 -0700

    fixed bugs in build files

--- build/beatAML/build_exp.sh
+++ build/beatAML/build_exp.sh
@@ -1,1 +1,1 @@
-python bGetBeatAML.py --exp --token $SYNAPSE_AUTH_TOKEN --curSamples $1 --drugFile $2
+python GetBeatAML.py --exp --token $SYNAPSE_AUTH_TOKEN --curSamples $1 --drugFile $2
--- build/test/test_docker.py
+++ build/test/test_docker.py
@@ -20,17 +20,39 @@ def main():
     args = parser.parse_args()
 
     ##make sure it can work with these test files
-    test_genes='test_genes.csv'
-    test_samples='test_samples.csv'
-    test_drugs='test_drugs.tsv'
+    test_genes='/tmp/test_genes.csv'
+    test_samples='/tmp/test_samples.csv'
+    test_drugs='/tmp/test_drugs.tsv'
 
 
-    ##get we generate samples?
+    dsname=args.docker
+    if dsname in ['broad_sanger_omics','broad_sanger_exp']:
+        dsname='broad_sanger;'
 
+    if args.samples:
+        ##get we generate sampless
+        cmd = 'docker run -v $PWD:/tmp '+args.docker+ ' sh build_samples.sh '+test_samples
+        print(cmd)
+        os.system(cmd)
 
-    ##can we generate omics with genes and new samples
+    if args.omics:
+        ##can we generate omics with genes and new samples
+        cmd = 'docker run -v $PWD:/tmp '+args.docker+ ' sh build_omics.sh '+test_genes+' /tmp/'+dsname+'_samples.csv'
+        print(cmd)
+        os.system(cmd)
 
-    ##can we generate drugs with existing drugs
+    if args.drugs:
+        ##can we generate drugs with existing drugs
+        cmd = 'docker run -v $PWD:/tmp '+args.docker+ ' sh build_drugs.sh '+test_drugs
+        print(cmd)
+        os.system(cmd)
 
+    if args.exp:
+        ##can we generate exp
+        cmd = 'docker run -v $PWD:/tmp '+args.docker+ ' sh build_exp.sh /tmp/'+dsname+'_samples.csv /tmp/'+dsname+'_drugs.tsv'
+        print(cmd)
+        os.system(cmd)
+  
 
-    ##can we generate exp
+if __name__=='__main__':
+    main()
