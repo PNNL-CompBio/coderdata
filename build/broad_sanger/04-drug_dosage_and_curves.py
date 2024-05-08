6e99c16bb933ddfaee18d9a87eb07f42333e6548
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-05-08 12:38:44 -0700

    update fit curve again

--- build/broad_sanger/04-drug_dosage_and_curves.py
+++ build/broad_sanger/04-drug_dosage_and_curves.py
@@ -13,6 +13,7 @@ TODO: add in R or dockerize requirements
 import os
 import argparse
 import pandas as pd
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--curSampleFile',dest='samplefile',default=None,help='DepMap sample file')
@@ -24,18 +25,18 @@ opts = parser.parse_args()
 samplefile = opts.samplefile
 drugfile = opts.dfile
 
-cmd = '/opt/venv/bin/python 04b-nci60-updated.py --sampleFile '+samplefile+' --drugFile '+drugfile
+cmd = ['/opt/venv/bin/python 04b-nci60-updated.py','--sampleFile',samplefile,'--drugFile',drugfile]
 print(cmd)
-os.system(cmd)
+subprocess.run(cmd)
 
 ####step 4a - get dose response data
-cmd = 'Rscript 04a-drugResponseData.R '+samplefile+' '+drugfile+' CTRPv2,FIMM,GDSC'
+cmd = ['Rscript','04a-drugResponseData.R',samplefile,drugfile,'CTRPv2,FIMM,GDSC']
 print(cmd)
-os.system(cmd)
+subprocess.run(cmd)
 
-cmd = 'Rscript 04a-drugResponseData.R '+samplefile+' '+drugfile+' gCSI,PRISM,CCLE'
+cmd = ['Rscript','04a-drugResponseData.R',samplefile,drugfile,'gCSI,PRISM,CCLE']
 print(cmd)
-os.system(cmd)
+subprocess.run(cmd)
 
 
 #cmd = 'Rscript 04a-drugResponseData.R '+samplefile+' '+drugfile+' NCI60'
@@ -46,7 +47,7 @@ os.system(cmd)
 allfiles=[a for a in os.listdir('./') if 'DoseResponse' in a]
 print(allfiles)
 for a in allfiles:
-    os.system('/opt/venv/bin/python fit_curve.py --input '+a+' --output '+a)
+    subprocess.run(['/opt/venv/bin/python','fit_curve.py','--input='+a+,'--output='+a])
 
 ###step 4c concatenate all files
 outfiles = [a for a in os.listdir("./") if ".0" in a]
