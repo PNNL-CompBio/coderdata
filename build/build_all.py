361ff97509efa142f2c034952c13161841cf9793
Jeremy <jeremy.jacobson3402@gmail.com>
2024-05-10 15:04:04 -0700

    Local Build Script Optimized for speed.

--- build/build_all.py
+++ build/build_all.py
@@ -2,32 +2,14 @@
 script that builds the coderdata package and stores locally
 """
 
-
 import os
 import argparse
+import time
+import random
 import subprocess
+from concurrent.futures import ThreadPoolExecutor
 
-def run_cmd(cmd_arr,filename):
-    '''
-    short command that runs command and collates output
-    '''
-    print('running...'+filename)
-    env = os.environ.copy()
-    if 'SYNAPSE_AUTH_TOKEN' not in env.keys():
-        print('You need to set the SYNAPSE_AUTH_TOKEN to acess the MPNST and beatAML Datasets')
-        docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','--platform=linux/amd64']
-    else:
-        docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','-e','SYNAPSE_AUTH_TOKEN='+env['SYNAPSE_AUTH_TOKEN'],'--platform=linux/amd64']
-    cmd = docker_run+cmd_arr
-    print(cmd)
-    res = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    if res.returncode !=0:
-        print(res.stderr)
-        exit(filename+' file failed')
-    else:
-        print(filename+' retrieved')
-            
-
+    
 def main():
     parser=argparse.ArgumentParser()
     parser.add_argument('--docker',dest='docker',default=False,action='store_true')
@@ -40,24 +22,47 @@ def main():
 
     args = parser.parse_args()
                     
-    ##make a 'local' directory for output
-    if not os.path.exists('local'):
-        os.mkdir('local')
-
-
-    #docker_run='docker run -v $PWD/local/:/tmp/ --platform=linux/amd64 
-    env = os.environ.copy()
-
-    ### build all docker images in parallel using docker compose.
-    if args.docker or args.all:
+    # Simulation command for testing order of everything:
+    # def run_docker_cmd(cmd_arr, filename):
+    #     # Simulate running the command by printing what would be run
+    #     print(f'Running: {filename} with command {" ".join(cmd_arr)}')
+    #     # Simulate execution time with a random delay
+    #     time.sleep(random.randint(1, 2))
+    #     print(f'Completed: {filename}')
+    
+    def run_docker_cmd(cmd_arr,filename):
+        '''
+        Essentially a wrapper for 'docker run'. Also provides output.
+        '''
+        print('running...'+filename)
+        env = os.environ.copy()
+        if 'SYNAPSE_AUTH_TOKEN' not in env.keys():
+            print('You need to set the SYNAPSE_AUTH_TOKEN to acess the MPNST and beatAML Datasets')
+            docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','--platform=linux/amd64']
+        else:
+            docker_run = ['docker','run','-v',env['PWD']+'/local/:/tmp/','-e','SYNAPSE_AUTH_TOKEN='+env['SYNAPSE_AUTH_TOKEN'],'--platform=linux/amd64']
+        cmd = docker_run+cmd_arr
+        print(cmd)
+        res = subprocess.run(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        if res.returncode !=0:
+            print(res.stderr)
+            exit(filename+' file failed')
+        else:
+            print(filename+' retrieved')
         
+    
+    def process_docker():
+        '''
+        Build all docker images using docker compose
+        All output and errors are logged at local/docker.log
+        '''
         compose_file = 'build/docker/docker-compose.yml'
         compose_command = ['docker-compose', '-f', compose_file, 'build', '--parallel']
-        
         log_file_path = 'local/docker.log'
+        env = os.environ.copy()
         with open(log_file_path, 'w') as log_file:
             # Execute the docker-compose command
-            res = subprocess.run(compose_command, env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+            res = subprocess.run(compose_command,  env=env, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
             # Log both stdout and stderr to the log file
             log_file.write(res.stdout)
             if res.returncode != 0:
@@ -69,75 +74,95 @@ def main():
                 print(f"Docker images built successfully. Details logged in {log_file_path}")
 
 
+    def process_drugs(executor, datasets):
+        '''
+        Build all drug files sequentially
+        '''
+        last_drug_future = None
+        dflist = []  
+        for da in datasets:
+            if da not in ['cptac', 'hcmi']:
+                di = 'broad_sanger_exp' if da == 'broad_sanger' else da
+                if not os.path.exists(f'local/{da}_drugs.tsv'):
+                    if last_drug_future:
+                        last_drug_future.result()  # Ensure the last drug process is completed before starting the next
+                    last_drug_future = executor.submit(run_docker_cmd, [di, 'sh', 'build_drugs.sh', ','.join(dflist)], f'{da} drugs')
+                    dflist.append(f'/tmp/{da}_drugs.tsv')
+    
+    def process_samples(executor, datasets):
+        '''
+        Build all samples files sequentially
+        '''
+        last_sample_future = None
+        sf = ''  
+        for da in datasets:
+                di = 'broad_sanger_omics' if da == 'broad_sanger' else da
+                if not os.path.exists(f'local/{da}_samples.csv'):
+                    if last_sample_future:
+                        last_sample_future.result() 
+                    last_sample_future = executor.submit(run_docker_cmd, [di, 'sh', 'build_samples.sh', sf], f'{da} samples')
+                    sf = f'/tmp/{da}_samples.csv'
+        
+    def process_omics(executor, datasets):
+        '''
+        Build all omics files concurrently
+        '''
+        for da in datasets:
+                di = 'broad_sanger_omics' if da == 'broad_sanger' else da
+                executor.submit(run_docker_cmd, [di, 'sh', 'build_omics.sh', '/tmp/genes.csv', f'/tmp/{da}_samples.csv'], f'{da} omics')
+    
+    
+    def process_experiments(executor, datasets):
+        '''
+        Build all experiments files concurrently
+        '''
+        for da in datasets:
+            if da not in ['cptac', 'hcmi']:
+                di = 'broad_sanger_exp' if da == 'broad_sanger' else da
+                if not os.path.exists(f'local/{da}_experiments.tsv'):
+                    executor.submit(run_docker_cmd, [di, 'sh', 'build_exp.sh', f'/tmp/{da}_samples.csv', f'/tmp/{da}_drugs.tsv'], f'{da} experiments')
+
+    ######
+    ### Begin Pipeline
+    #####
+    
+    # Make a 'local' directory for output
+    if not os.path.exists('local'):
+        os.mkdir('local')
+
+    # Get dataset names - default is all.
     datasets = args.datasets.split(',')
 
+    ### Build Docker Images. These are all built in Parallel. Nothing else can run until these are built.
+    # Ouput is logged at local/docker.log
+    if args.docker or args.all:
+        process_docker()
 
-    ### Any new sample creation must happened here.
-    ### Each sample file requires the previous one to be created
-    ### current order is : DepMap, Sanger, CPTAC, HCMI, BeatAML, MPNST
-    ## can be run independently but first before omics/experiemnts
-    if args.samples or args.all:
-        ### build gene file
-        if not os.path.exists('/tmp/genes.csv'):
-            run_cmd(['genes','sh','build_genes.sh'],'gene file')
-        
-        ###build sample files
-        sf=''
-        for da in datasets:#['broad_sanger_omics','cptac','hcmi','beataml','mpnst']:
-            if da=='broad_sanger':
-                di = 'broad_sanger_omics'
-            else:
-                di = da
-            if not os.path.exists('local/'+da+'_samples.csv'):
-                run_cmd([di,'sh','build_samples.sh',sf],da+' samples')
-            sf = '/tmp/'+da+'_samples.csv' ##TODO make this into a list
-
-                
-
-     ### Drug matching scripts take a while
-    ### they are their own step and can be run independentyly, before others, or alongside sample/omics
-    ### DepMap/Sanger, MPNST, LINCS
-    dflist=[]
-    if args.drugs or args.all:
-        ###build drug data
-        for da in [a for a in datasets if a not in ['cptac','hcmi']]:
-            if da == 'broad_sanger':
-                di = 'broad_sanger_exp'
-            else:
-                di = da
-                
-            if not os.path.exists('local/'+da+'_drugs.tsv'):
-                run_cmd([di,'sh','build_drugs.sh',','.join(dflist)],da+' drugs')
-            dflist = dflist.append('/tmp/'+da+'_drugs.tsv')
+    ### Build Drugs files and Samples files. These two steps are run in Parallel. 
+    ### Within each step, sequential running is required.
+    with ThreadPoolExecutor() as executor:
+        if args.samples or args.all:
+            sample_thread = executor.submit(process_samples,executor, datasets)
+        if args.drugs or args.all:
+            drug_thread = executor.submit(process_drugs,executor, datasets)
             
+        # Wait for both processes to complete before proceeding to omics and experiments
+        drug_thread.result()
+        sample_thread.result()
+
+
+    ### At this point in the pipeline, all samples and drugs files have been created. There are no blockers to proceed.
+    ### Build Omics files and Experiments files. These two steps are run in Parallel. 
+    ### Within each step, all datasets are run in Parallel.
+    with ThreadPoolExecutor() as executor:
+        if args.omics or args.all:
+            omics_thread = executor.submit(process_omics, executor, datasets)
+        if args.exp or args.all:
+            exp_thread = executor.submit(process_experiments, executor, datasets)
             
-
-    #### Any new omics files are created here.
-    ## depends on samples!
-    ### these are not order dependent but require gene and sample files
-    if args.omics or args.all:
-        ###depmap cell line
-        for da in datasets:
-            if da == 'broad_sanger':
-                di = 'broad_sanger_omics'
-            else:
-                di = da
-            run_cmd([di,'sh','build_omics.sh','/tmp/genes.csv','/tmp/'+da+'_samples.csv'],da+' omics')
+        omics_thread.result()
+        exp_thread.result()
 
 
-    ### drug response data
-    ## requires samplesa nd drugs to complete
-    if args.exp or args.all:
-        for da in [a for a in datasets if a not in ['cptac','hcmi']]:
-            if da == 'broad_sanger':
-                di = 'broad_sanger_exp'
-            else:
-                di = da
-            if not os.path.exists('local/'+da+'_experiments.tsv'):
-                run_cmd([di,'sh','build_exp.sh','/tmp/'+df+'_samples.csv','/tmp/'+da+'_drugs.tsv'],da+' experiments')
-    
-
-
-
-
-main()
+if __name__ == '__main__':
+    main()
