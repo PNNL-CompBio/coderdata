68ba28a1ab70aa3c81772c4ca18eb1c6c60dbb1c
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-06-11 09:06:09 -0700

    Updated Docker and build scripts

--- build/mpnst/build_drugs.sh
+++ build/mpnst/build_drugs.sh
@@ -1,1 +1,2 @@
 Rscript 02_get_drug_data.R $SYNAPSE_AUTH_TOKEN $1 /tmp/mpnst_drugs.tsv
+/opt/venv/bin/python build_drug_desc.py --drugtable /tmp/mpnst_drugs.tsv --desctable /tmp/mpnst_desciptors.tsv
