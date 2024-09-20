0866762d91402bd4e0f98f1e34cdf6e346fcd94a
Jeremy <jeremy.jacobson3402@gmail.com>
2024-09-20 14:23:28 -0700

    removing devtools from requirements

--- build/mpnst/requirements.r
+++ build/mpnst/requirements.r
@@ -1,7 +1,3 @@
-# Install the rjson package at version 0.2.21
-devtools::install_version("rjson", version = "0.2.21")
-
-# Install other required packages
 install.packages("dplyr")
 install.packages("data.table")
 install.packages("synapser", repos = c("http://ran.synapse.org", "https://cloud.r-project.org"))
