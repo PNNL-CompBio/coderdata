02b65aac0c64142c2ea67972b1234386baa4aa07
Jeremy <jeremy.jacobson3402@gmail.com>
2024-11-13 14:53:48 -0800

    Added pre-split script to remove problematic drugs

--- build/broad_sanger/build_misc.sh
+++ build/broad_sanger/build_misc.sh
@@ -4,8 +4,12 @@ set -euo pipefail
 trap 'echo "Error on or near line $LINENO while executing: $BASH_COMMAND"; exit 1' ERR
 
 cp /tmp/broad_sanger* .
-echo "Running 05_separate_datasets.py..."
-/opt/venv/bin/python 05_separate_datasets.py
+
+echo "Running 05a_remove_problem_drugs.py..."
+/opt/venv/bin/python 05a_remove_problem_drugs.py
+
+echo "Running 05b_separate_datasets.py..."
+/opt/venv/bin/python 05b_separate_datasets.py
 
 echo "Removing broad_sanger* files..."
 rm broad_sanger*
