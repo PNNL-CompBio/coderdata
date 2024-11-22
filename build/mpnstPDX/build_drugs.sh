1680f41af4b2692d1cdff92e8192e2460cd068f9
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-11-22 11:10:53 -0800

    updated

--- build/mpnstPDX/build_drugs.sh
+++ build/mpnstPDX/build_drugs.sh
@@ -1,2 +1,4 @@
+##get drug data
 Rscript 02_get_drug_data.R $SYNAPSE_AUTH_TOKEN $1 /tmp/mpnstpdx_drugs.tsv
+##get drug descriptors
 /opt/venv/bin/python3 build_drug_desc.py --drugtable /tmp/mpnstpdx_drugs.tsv --desctable /tmp/mpnstpdx_drug_descriptors.tsv.gz
