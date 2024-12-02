bf4aaac836da0f51367b2fffe87ea9a6c366903c
Sara JC Gosline <sara.gosline@pnnl.gov>
2024-12-02 14:35:24 -0800

    updated with new drug search

--- build/pancpdo/03-getPancPDODrugs.py
+++ build/pancpdo/03-getPancPDODrugs.py
@@ -10,12 +10,39 @@ import argparse
 filelink='https://aacr.figshare.com/ndownloader/files/39996295'
 ##get third tab and drugsa re listeda cross top
 
+
+    
+
+def retrieve_figshare_data(url):
+    """
+    Download data from a given Figshare URL.
+    
+    Parameters
+    ----------
+    url : string
+        The Figshare URL to download data from.
+    
+    Returns
+    -------
+    string
+        Name of the downloaded file.
+    """
+    
+    files_0 = os.listdir()
+    wget.download(url)
+    files_1 = os.listdir()
+    new_file = str(next(iter(set(files_1) - set(files_0))))
+    return new_file
+
 def main():
     parser = argparse.ArgumentParser(description='Download and match pancpdocdrugs')
     parser.add_argument('-d', '--prevDrugFile')
     parser.add_argument('-o', '--output', default = '/tmp/panpdc_drugs.tsv')
 
-    
+    auc_file = retrieve_figshare_data(filelink)
 
+    tab = pd.read_excel(auc_file,sheet='')
+    
+    
 if __name__=='__main__':
     main()
