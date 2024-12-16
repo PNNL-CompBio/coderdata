28ea7e3b6d9e2f83ce6e6556e6dfa712e7873d74
Yannick Mahlich <yannick.mahlich@pnnl.gov>
2024-12-16 11:42:50 -0800

    removed coderdata.DatasetLoader / coderdata.loader.*

--- coderdata/__init__.py
+++ coderdata/__init__.py
@@ -1,5 +1,4 @@
 from .download.downloader import download
-from .load.loader import DatasetLoader, join_datasets
 from .split.splitter import train_test_validate
 from .dataset.dataset import (
     Dataset,
