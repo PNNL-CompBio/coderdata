f21311da9f636839c387559e6600ba8892fd520f
Yannick Mahlich <yannick.mahlich@pnnl.gov>
2025-01-08 14:00:22 -0800

    updated cli to use updated download functionality

--- coderdata/cli.py
+++ coderdata/cli.py
@@ -1,5 +1,6 @@
 import argparse
-from .download.downloader import download
+import sys
+from .download.downloader import download as download_datasets
 
 def main():
     parser = argparse.ArgumentParser(prog='coderdata')
@@ -7,20 +8,25 @@ def main():
 
     # Subcommand 'download'
     parser_download = subparsers.add_parser('download', help='Download datasets')
-    parser_download.add_argument('--prefix', type=str, default=None,
-                                 help='Prefix of the dataset to download (e.g., "hcmi"), "all", or leave empty for all files.')
+    parser_download.add_argument(
+        '-n', '--name',
+        dest='DATASET_NAME',
+        type=str,
+        default='all',
+        help='Name of the dataset to download (e.g., "beataml"). '
+             'Alternatively "all" will download the full repository of '
+             'coderdata datasets. See "coderdata --list" for a complete list '
+             'of available datasets. Defaults to "all"'
+    )
     parser_download.set_defaults(func=download)
-
+    if len(sys.argv) == 1:
+        parser.print_help(sys.stderr)
+        sys.exit(0)
     args = parser.parse_args()
-    if hasattr(args, 'func'):
-        # Check if 'prefix' argument is present and pass it directly to the function
-        if 'prefix' in args:
-            args.func(args.prefix)
-        else:
-            args.func()
-    else:
-        parser.print_help()
+    args.func(args)
 
+def download(args):
+    download_datasets(name=args.DATASET_NAME)
 if __name__ == '__main__':
     main()
     
