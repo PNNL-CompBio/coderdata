0aff709d18e826888d28a90e244b412214268d2d
alexandriai168 <alexandria.kay.im@gmail.com>
2025-04-09 10:35:00 -0700

    added drug descriptor

--- build/crc_organoids/build_drugs.sh
+++ build/crc_organoids/build_drugs.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 trap 'echo "Error on or near line $LINENO while executing: $BASH_COMMAND"; exit 1' ERR
 
+# running the drug python script
 echo "Running 03-drug-crc.py with token and prevSamples $1."
 python 03-drug-crc.py --Download --Drugs --token $SYNAPSE_AUTH_TOKEN --prevSamples $1
 
+# running the drug descriptor python script
+python build_drug_desc.py --drugtable /tmp/crc_drugs.csv --desctable /tmp/crc_drug_descriptors.csv.gz
