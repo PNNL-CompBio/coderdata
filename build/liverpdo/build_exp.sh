2ebfa3876549d1eb0c99dedae339fbbd12127b19
alexandriai168 <alexandria.kay.im@gmail.com>
2025-07-01 12:22:25 -0700

    created build exp shell scrtip

---
+++ build/liverpdo/build_exp.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+
+trap 'echo "Error on or near line $LINENO while executing: $BASH_COMMAND"; exit 1' ERR
+
+# running the drug python script
+echo "Running 04-experiments-crcpdo.py with token, samples file $1 and drugs file $2."
+python3 04-experiments-crcpdo.py --Download --Experiment --Token $SYNAPSE_AUTH_TOKEN --Samples $1 --Drugs $2
+
+# running the drug descriptor python script
+python3 fit_curve.py --input /tmp/crcpdo_experiments_for_curve_fitting.tsv --output /tmp/crcpdo_experiments.tsv
+
+# change name of script 
+mv /tmp/crcpdo_experiments.tsv.0 /tmp/crcpdo_experiments.tsv
