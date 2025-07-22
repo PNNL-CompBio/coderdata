99e8144b859001ec5e1dd9f65322cf725911260a
alexandriai168 <alexandria.kay.im@gmail.com>
2025-07-22 16:09:26 -0700

    there was a bug with command

--- build/novartispdx/build_exp.sh
+++ build/novartispdx/build_exp.sh
@@ -1,5 +1,5 @@
 
 #python3 04-experiments-novartispdx.py --token $SYNAPSE_AUTH_TOKEN 
 
-python3 -m novartispdx.04-experiments-novartispdx --token $SYNAPSE_AUTH_TOKEN -o ~/Projects/CoderData/dev-environment/novartispdx/novartispdx_curvedata.tsv
+python3 -m 04-experiments-novartispdx --token $SYNAPSE_AUTH_TOKEN -o ~/Projects/CoderData/dev-environment/novartispdx/novartispdx_curvedata.tsv
 python3 utils/calc_pdx_metrics.py /tmp/novartispdx_curvedata.tsv --drugfile=/tmp/novartispdx_drugs.tsv --outprefix=/tmp/novartispdx --study='Novartis PDX Gao etal 2015' --source='Synapse'
