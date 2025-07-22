4af75d8c85a967a79c0c169976526152ce24b07c
alexandriai168 <alexandria.kay.im@gmail.com>
2025-07-22 16:44:09 -0700

    fixing issue with filepath

--- build/novartispdx/build_drugs.sh
+++ build/novartispdx/build_drugs.sh
@@ -5,7 +5,7 @@ trap 'echo "Error on or near line $LINENO while executing: $BASH_COMMAND"; exit 
 
 echo "Running script with token and drugFile $1"
 # for running locally (from build directory):
-python3 -m novartispdx.03-drugs-novartispdx --token $SYNAPSE_AUTH_TOKEN -d $1 -o /tmp/novartispdx_drugs.tsv
+python3 -m 03-drugs-novartispdx --token $SYNAPSE_AUTH_TOKEN -d $1 -o /tmp/novartispdx_drugs.tsv
 #python3 novar
 #python3 03-drugs-novartispdx.py --token $SYNAPSE_AUTH_TOKEN -d $1 -o /tmp/novartispdx_drugs.tsv
 
